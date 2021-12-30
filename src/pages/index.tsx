7353cd03627d64e987cc35cb7e4a5c30343c03a9
Fillipi Pereira <pfillipi@gmx.de>
2021-12-30 04:31:53 +0100

    fetchNextPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -62,7 +62,16 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage()}
+            disabled={isFetchingNextPage}
+            mt="6"
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
